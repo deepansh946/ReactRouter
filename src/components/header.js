f741797a35b79b6dd92047997e58835d8880dda7
Deepansh Bhargava <deepansh940@gmail.com>
2017-09-07 13:31:50 +0530

    Added new files

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1><center>TODO APP</center></h1>
+      </div>
+    );
+  }
+}
