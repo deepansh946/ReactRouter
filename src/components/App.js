2efd5bee40fd5d14a3b8f29601519483959605c8
Deepansh Bhargava <deepansh940@gmail.com>
2017-09-07 13:30:39 +0530

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import List from './list.js';
+import Header from './header.js';
 
-class App extends Component {
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      items: []
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({ term: event.target.value });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    });
+  }
+
   render() {
     return (
-      <div >
-        hii shaurya
+      <div>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Submit</button>
+        </form>
+        <List items={this.state.items} />
       </div>
     );
   }
 }
-
-export default App;
